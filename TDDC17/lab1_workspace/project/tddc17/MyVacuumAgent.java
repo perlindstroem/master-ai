548fb8219ebd65c1536b8f8b24373633aeb9ed06
EliasAlesand <elial148@student.liu.se>
2017-09-12 17:30:09 +0200

    Done with lab1

--- TDDC17/lab1_workspace/project/tddc17/MyVacuumAgent.java
+++ TDDC17/lab1_workspace/project/tddc17/MyVacuumAgent.java
@@ -2,27 +2,26 @@ package tddc17;
 
 
 import aima.core.environment.liuvacuum.*;
+import sun.management.resources.agent;
 import aima.core.agent.Action;
 import aima.core.agent.AgentProgram;
 import aima.core.agent.Percept;
 import aima.core.agent.impl.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
-import java.util.Set;
 import java.util.Stack;
-
-class Coord{
+class Coordinate{
 	public int x;
 	public int y;
+	public Coordinate parent;
 	
-	public Coord(int x, int y){
+	public Coordinate(int x, int y, Coordinate parent){
 		this.x = x;
 		this.y = y;
+		this.parent = parent;
 	}
 	
 	@Override
@@ -47,7 +46,7 @@ class Coord{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Coord other = (Coord) obj;
+		Coordinate other = (Coordinate) obj;
 		if (x != other.x)
 			return false;
 		if (y != other.y)
@@ -55,7 +54,6 @@ class Coord{
 		return true;
 	}
 }
-
 class MyAgentState
 {
 	public int[][] world = new int[30][30];
@@ -150,15 +148,13 @@ class MyAgentProgram implements AgentProgram {
 	public int iterationCounter = 1800;
 	public MyAgentState state = new MyAgentState();
 	
-	public boolean followGoal = false;
-	public int goal_x_position;
-	public int goal_y_position;
+	public Queue<Coordinate> path = new LinkedList<Coordinate>();
+	public HashSet<Coordinate> unknownNodes = new HashSet<Coordinate>();
+	
+	Coordinate goal;
+	Coordinate current;
+	
 	public boolean cleaningDone = false;
-	public int eastWall = 30, southWall = 30;
-	public Queue<Coord> searchQueue = new LinkedList<Coord>();
-	public Stack<Coord> goalStack = new Stack<Coord>();
-	public Set<Coord> visited = new HashSet<Coord>();
-	public Set<Coord> obstacles = new HashSet<Coord>();
 	
 	// moves the Agent to a random start position
 	// uses percepts to update the Agent position - only the position, other percepts are ignored
@@ -185,7 +181,7 @@ class MyAgentProgram implements AgentProgram {
 	
 	@Override
 	public Action execute(Percept percept) {
-		
+		current = new Coordinate(state.agent_x_position, state.agent_y_position, null);
 		// DO NOT REMOVE this if condition!!!
     	if (initnialRandomActions>0) {
     		return moveToRandomStartPosition((DynamicPercept) percept);
@@ -204,6 +200,7 @@ class MyAgentProgram implements AgentProgram {
     	System.out.println("x=" + state.agent_x_position);
     	System.out.println("y=" + state.agent_y_position);
     	System.out.println("dir=" + state.agent_direction);
+    	
     	
 		
 	    iterationCounter--;
@@ -242,304 +239,208 @@ class MyAgentProgram implements AgentProgram {
 	    
 	    state.printWorldDebug();
 	    
-	    //state.agent_last_action=state.ACTION_NONE;
-    	//return NoOpAction.NO_OP;
-	    
 	    // Next action selection based on the percept value
-	    System.out.println("SearchQueue: " + searchQueue);
-	    
-	    visited.add(new Coord(state.agent_x_position, state.agent_y_position));
-	    queueNearbyCoords();
-	    
-	    if(!followGoal){
-	    	try{
-	    		Coord goal = searchQueue.remove();
-	    		System.out.println("New goal: " + goal);
-	    		setGoal(goal);
-	    	}catch(Exception e){
-	    		cleaningDone = true;
-	    		setGoal(new Coord(1,1));
-	    	}
-	    } else {
-	    	Coord goal = new Coord(goal_x_position, goal_y_position);
-	    	System.out.println("Current goal: " + goal);
-	    }
-	    
 	    if (dirt)
 	    {
 	    	System.out.println("DIRT -> choosing SUCK action!");
 	    	state.agent_last_action=state.ACTION_SUCK;
 	    	return LIUVacuumEnvironment.ACTION_SUCK;
 	    } 
-	    
-	    if (home && cleaningDone){
+	    addNearbyCoords();
+	    Coordinate c = findUnknown();
+	    if (home && c == null){
 	    	state.agent_last_action=state.ACTION_NONE;
 	    	return NoOpAction.NO_OP;
 	    }
 	    
-	    if (bump) {
-			System.out.println("BUMPED");
-			int next_x_position = state.agent_x_position;
-			int next_y_position = state.agent_y_position;
-			
-			if(state.agent_direction == MyAgentState.EAST ){
-				next_x_position = state.agent_x_position+1;
-				next_y_position = state.agent_y_position;
-			}
-			if(state.agent_direction == MyAgentState.NORTH){
-				next_x_position = state.agent_x_position;
-				next_y_position = state.agent_y_position-1;
-			}
-			if(state.agent_direction == MyAgentState.WEST){
-				next_x_position = state.agent_x_position-1;
-				next_y_position = state.agent_y_position;
-			}
-			if(state.agent_direction == MyAgentState.SOUTH){
-				next_x_position = state.agent_x_position;
-				next_y_position = state.agent_y_position+1;
-			}
-			
-			if(goal_x_position == next_x_position && goal_y_position == next_y_position){
-				followGoal = false;
-			}
-			
-			obstacles.add(new Coord(next_x_position, next_y_position));
-			
-			/*if (state.agent_direction == MyAgentState.EAST)
-				eastWall = state.agent_x_position + 1;
-			else if (state.agent_direction == MyAgentState.SOUTH)
-				southWall = state.agent_y_position + 1;*/
-			
-			
-			state.agent_direction = (state.agent_direction +1)%4;
-			state.agent_last_action=state.ACTION_TURN_RIGHT;
-			return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
+	    if (path.isEmpty() || bump) {
+	    	
+	    	if (c == null) {
+	    		path = pathFinder(new Coordinate(1,1, null));
+	    		goal = path.remove();
+	    	}
+	    	else {
+	    		
+	    		path = pathFinder(c);
+	    		goal = path.remove();
+	    	}
 		}
-		/*else
-		{
-			if(forwardCheck() == 2){
-				state.agent_direction = (state.agent_direction +1)%4;
-	    		state.agent_last_action=state.ACTION_TURN_RIGHT;
-	    		return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
-			}else{
-				state.agent_last_action=state.ACTION_MOVE_FORWARD;
-				return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-			}
-			
-		}*/
-	    
-	    if(followGoal){
-	    	if(state.agent_x_position < goal_x_position){
-	    		if(state.agent_direction != MyAgentState.EAST){
-	    			state.agent_direction = (state.agent_direction +1)%4;
-		    		state.agent_last_action=state.ACTION_TURN_RIGHT;
-		    		return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
-	    		}
-	    		state.agent_last_action=state.ACTION_MOVE_FORWARD;
-    			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-	    	}
-	    	else if(state.agent_x_position > goal_x_position){
-	    		if(state.agent_direction != MyAgentState.WEST){
-	    			state.agent_direction = (state.agent_direction +1)%4;
-		    		state.agent_last_action=state.ACTION_TURN_RIGHT;
-		    		return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
-	    		}
-	    		state.agent_last_action=state.ACTION_MOVE_FORWARD;
-    			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-	    	}
-	    	else if(state.agent_y_position > goal_y_position){
-	    		if(state.agent_direction != MyAgentState.NORTH){
-	    			state.agent_direction = (state.agent_direction +1)%4;
-		    		state.agent_last_action=state.ACTION_TURN_RIGHT;
-		    		return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
-	    		}
-	    		state.agent_last_action=state.ACTION_MOVE_FORWARD;
-    			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-	    	}
-	    	else if(state.agent_y_position < goal_y_position){
-	    		if(state.agent_direction != MyAgentState.SOUTH){
-	    			state.agent_direction = (state.agent_direction +1)%4;
-		    		state.agent_last_action=state.ACTION_TURN_RIGHT;
-		    		return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
-	    		}
-	    		state.agent_last_action=state.ACTION_MOVE_FORWARD;
-    			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-	    	}
-	    	else{
-	    		//goal was found
-	    		followGoal = false;
-	    	}
+	    if (state.agent_x_position == goal.x && state.agent_y_position == goal.y) {
+	    	goal = path.remove();
 	    }
-	    
-	    /*if(isCorneredCheck()){
-	    	if (isDone()){
-	    		cleaningDone = true;
-	    		setGoal(1,1);
-	    	}
-	    }*/
-	    
-	    state.agent_last_action=state.ACTION_MOVE_FORWARD;
-		return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
-	}
-	
-	Queue<Coord> calculatePath(Coord start, Coord goal){
-		Set<Coord> closedSet = new HashSet<Coord>();
-		Set<Coord> openSet = new HashSet<Coord>();
-		openSet.add(start);
-		
-		Map<Coord, Coord> cameFrom = new HashMap<Coord, Coord>();
-		Map<Coord, Integer> gScore = new HashMap<Coord, Integer>();
-		gScore.put(start, 0);
-		
-		Map<Coord, Double> fScore = new HashMap<Coord, Double>();
-		fScore.put(start, heuristicCost(start,goal));
-		
-		Coord[] nearby = new Coord[] {
-				new Coord(start.x+1, start.y),
-				new Coord(start.x-1, start.y),
-				new Coord(start.x, start.y+1),
-				new Coord(start.x, start.y-1)
-		};
-		
-		while(!openSet.isEmpty()){
-			Coord current = getLowestScore(openSet, fScore);
-			
-			if(current == goal){
-				return reconstructPath(cameFrom, current);
+		if (state.agent_x_position < goal.x ) {
+			if (state.agent_direction != MyAgentState.EAST) {
+				return rotate(MyAgentState.EAST);
 			}
-			
-			openSet.remove(current);
-			closedSet.add(current);
-			
-			for(Coord neighbour : nearby){
-				if(closedSet.contains(neighbour) || obstacles.contains(neighbour)){
-					continue;
-				}
-				
-				if(!openSet.contains(neighbour)){
-					openSet.add(neighbour);
-				}
-				
-				//fortsätt här
-				//tentativeGScore = 
+			state.agent_last_action = state.ACTION_MOVE_FORWARD;
+			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
+		} else if (state.agent_x_position > goal.x) {
+			if (state.agent_direction != MyAgentState.WEST) {
+				return rotate(MyAgentState.WEST);
 			}
-			
+			state.agent_last_action = state.ACTION_MOVE_FORWARD;
+			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
+		} else if (state.agent_y_position > goal.y) {
+			if (state.agent_direction != MyAgentState.NORTH) {
+				return rotate(MyAgentState.NORTH);
+			}
+			state.agent_last_action = state.ACTION_MOVE_FORWARD;
+			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
+		} else if (state.agent_y_position < goal.y) {
+			if (state.agent_direction != MyAgentState.SOUTH) {
+				return rotate(MyAgentState.SOUTH);
+			}
+			state.agent_last_action = state.ACTION_MOVE_FORWARD;
+			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
+		} else {
+			return LIUVacuumEnvironment.ACTION_MOVE_FORWARD;
 		}
-		
-		return null;
+
 	}
-	
-	Coord[] getNeighbours(Coord coord){
-		return new Coord[] {
-				new Coord(coord.x+1, coord.y),
-				new Coord(coord.x-1, coord.y),
-				new Coord(coord.x, coord.y+1),
-				new Coord(coord.x, coord.y-1)
-		};
+	/***
+	 * Performs a breadth first search to to find the shortest path to coordinate c.
+	 * @param c Goal coordinate.
+	 * @return Returns a queue of coordinates that is the path to coordinate c. 
+	 */
+	Queue<Coordinate> pathFinder(Coordinate c) {
+		Queue<Coordinate> queue = new LinkedList<Coordinate>();
+		HashSet<Coordinate> visited = new HashSet<Coordinate>();
+	    queue.add(new Coordinate(state.agent_x_position,  state.agent_y_position, null));
 		
+	    while(!queue.isEmpty()){
+	        Coordinate node = queue.remove();
+	        visited.add(node);
+	        
+	        if (node.x == c.x && node.y == c.y) {
+	        	return getPathFromChild(node);
+	        }
+	        
+	        if (node.x!=30) {
+	        	Coordinate t = new Coordinate(node.x+1,node.y,node);
+	        	if (state.world[t.x][t.y] != 1 && !visited.contains(t))
+	        		queue.add(t);
+	        }
+	        if (node.x!=0){
+	        	Coordinate t = new Coordinate(node.x-1,node.y,node);
+	        	if (state.world[t.x][t.y] != 1 && !visited.contains(t))
+	        		queue.add(t);
+	        }
+	        if (node.y!=30){
+	        	Coordinate t = new Coordinate(node.x,node.y+1,node);
+	        	if (state.world[t.x][t.y] != 1 && !visited.contains(t))
+	        		queue.add(t);
+	        }
+	        if (node.y!=0){
+	        	Coordinate t = new Coordinate(node.x,node.y-1,node);
+	        	if (state.world[t.x][t.y] != 1 && !visited.contains(t))
+	        		queue.add(t);
+	        }
+	    }
+	    return null;
 	}
-	
-	Coord getLowestScore(Set<Coord> set, Map<Coord, Double> score){
-		Coord minNode = null;
-		
-		double minScore = 1000;
-		for(Coord coord : set){
-			if(score.get(coord) < minScore){
-				minNode = coord;
+	/***
+	 * Backtracks the path from a child to the start node.
+	 * @param child The goal coordinate.
+	 * @return Returns the queue of coordinates.
+	 */
+	Queue<Coordinate> getPathFromChild(Coordinate child){
+		Stack<Coordinate> stack = new Stack<Coordinate>();
+		while (child.parent != null) {
+			stack.push(child);
+			child = child.parent;
+		}
+		Queue<Coordinate> queue = new LinkedList<Coordinate>();
+		while(!stack.isEmpty()) {
+			queue.add(stack.pop());
+		}
+		return queue;
+	}
+	/***
+	 * Finds the closest unknown node.
+	 * Uses pythagoras theorem to estimate the closest node.
+	 * @return
+	 */
+	Coordinate findUnknown() {
+		Coordinate closest = null;
+		double minDistance = 1000;
+		HashSet<Coordinate> remove = new HashSet<Coordinate>();
+		for (Coordinate c : unknownNodes) {
+			if (state.world[c.x][c.y] != 0) {
+				remove.add(c);
+				continue;
+			}
+			double d = distance(c, new Coordinate(state.agent_x_position, state.agent_y_position, null));
+			if (d < minDistance){
+				closest = c;
+				minDistance = d;
 			}
 		}
-		
-		return null;
+		for (Coordinate c : remove) {
+			unknownNodes.remove(c);
+		}
+		return closest;
 	}
-	
-	double heuristicCost(Coord a, Coord b){
+	/***
+	 * Adds nearby unvisited nodes to a set.
+	 */
+	void addNearbyCoords(){
+		int x = state.agent_x_position;
+		int y = state.agent_y_position;
+		
+		if (state.world[x+1][y] == 0)
+			unknownNodes.add((new Coordinate(x+1,y, null)));
+		if (state.world[x-1][y] == 0)
+			unknownNodes.add((new Coordinate(x-1,y, null)));
+		if (state.world[x][y+1] == 0)
+			unknownNodes.add((new Coordinate(x,y+1, null)));
+		if (state.world[x][y-1] == 0)
+			unknownNodes.add((new Coordinate(x,y-1, null)));
+		
+		
+	}
+	/***
+	 * Calculates the distance between two coordinates using pythagoras theorem.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	double distance(Coordinate a, Coordinate b){
 		int dx = Math.abs(a.x - b.x);
 		int dy = Math.abs(a.y - b.y);
 		
 		return Math.sqrt((dx*dx) + (dy*dy));
 	}
-	
-	Queue<Coord> reconstructPath(Map<Coord, Coord> cameFrom, Coord current){
+	/***
+	 * Rotates towards a goal direction by calculating the quickest way to rotate.
+	 * @param goalDirection
+	 * @return
+	 */
+	Action rotate(int goalDirection) {
+		int currDirection = state.agent_direction;
+		int left = currDirection, right = currDirection;
+		int leftTurns = 0, rightTurns = 0;
 		
-		
-		return null;
-	}
-	
-	int forwardCheck() {
-		switch (state.agent_direction) {
-		case MyAgentState.NORTH:
-			return state.world[state.agent_x_position][state.agent_y_position-1];
-		case MyAgentState.EAST:
-			return state.world[state.agent_x_position+1][state.agent_y_position];
-		case MyAgentState.SOUTH:
-			return state.world[state.agent_x_position][state.agent_y_position+1];
-		case MyAgentState.WEST:
-			return state.world[state.agent_x_position-1][state.agent_y_position];
+		while(left != goalDirection) {
+			left = (left == 0 ? 3 : left - 1);
+			leftTurns++;
 		}
-		return -1;
-	}
-	
-	boolean isCorneredCheck() {
-		if(state.world[state.agent_x_position][state.agent_y_position-1] != 0 &&
-			state.world[state.agent_x_position][state.agent_y_position+1] != 0 &&
-			state.world[state.agent_x_position-1][state.agent_y_position] != 0 &&
-			state.world[state.agent_x_position+1][state.agent_y_position] != 0)
-			return true;
-		return false;
-	}
-	
-	void removeGoalIfBlocked(){
-		//TODO
-	}
-	
-	void setGoal(int x, int y) {
-		followGoal = true;
-		goal_x_position = x;
-		goal_y_position = y;
-	}
-	
-	void setGoal(Coord coord) {
-		followGoal = true;
-		goal_x_position = coord.x;
-		goal_y_position = coord.y;
-	}
-	
-	boolean isDone(){
-		for(int i = 1; i < eastWall-1;i++){
-			for(int j = 1; j < southWall-1;j++ ){
-				if (state.world[i][j] == 0){
-					setGoal(i,j);
-					return false;
-				}
-			}
+		while(right != goalDirection) {
+			right = (right + 1) % 4;
+			rightTurns++;
 		}
-		return true;
-	}
-	
-	void queueNearbyCoords(){
-		int x = state.agent_x_position;
-		int y = state.agent_y_position;
 		
-		queueCoord(new Coord(x,y-1));
-		//queueCoord(new Coord(x+1,y-1));
-		queueCoord(new Coord(x+1,y));
-		//queueCoord(new Coord(x+1,y+1));
-		queueCoord(new Coord(x,y+1));
-		//queueCoord(new Coord(x-1,y+1));
-		queueCoord(new Coord(x-1,y));
-		//queueCoord(new Coord(x-1,y-1));
-		
-	}
-	
-	void queueCoord(Coord coord){
-		if(!visited.contains(coord) && !searchQueue.contains(coord) && !obstacles.contains(coord)){
-			searchQueue.add(coord);
-		} else {
-			System.out.println("Tried to add visisted: " + coord);
+		if(leftTurns < rightTurns) {
+			state.agent_direction = (state.agent_direction == 0 ? 3 : state.agent_direction - 1);
+			state.agent_last_action = state.ACTION_TURN_LEFT;
+			return LIUVacuumEnvironment.ACTION_TURN_LEFT;
+		}else {
+			state.agent_direction = (state.agent_direction + 1) % 4;
+			state.agent_last_action = state.ACTION_TURN_RIGHT;
+			return LIUVacuumEnvironment.ACTION_TURN_RIGHT;
 		}
 	}
+	
 }
-
 public class MyVacuumAgent extends AbstractAgent {
     public MyVacuumAgent() {
     	super(new MyAgentProgram());
